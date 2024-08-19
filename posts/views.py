52cbcb962dc04a1652ee1bf873b457a1a1ca04f9
Azamat222008 <sagyndykov.azamat.09@gmail.com>
2024-08-20 01:10:22 +0600

    4month

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from posts.models import Post
 
 
 def hello_world(request):
@@ -8,3 +9,14 @@ def hello_world(request):
 
 def main_page_view(request):
     return render(request, 'main_page.html')
+
+
+def post_list_view(request):
+    posts = Post.objects.all()
+    return render(request, 'post_list.html', context={'posts': posts})
+
+
+def post_detail_view(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, 'post_detail.html', context={'post': post})
+
